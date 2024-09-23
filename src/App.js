a841446a35677d25b39f6c868280946aa02710a8
Гуров Евгений Александрович <gurovea@srvhub.ru>
2024-09-23 16:27:02 +0300

    добавил вывод initData

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import WebApp from '@twa-dev/sdk';
 
 function App() {
+    alert(WebApp.initData);
   return (
     <div className="App">
       <header className="App-header">
